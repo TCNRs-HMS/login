5e8f412ed622cb2a5acf07d3e19d93961ac0fcf3
GROUP 3_Dalusung, Festejo, Isip, Sutare <153100461+TCNRs-HMS@users.noreply.github.com>
2024-11-18 13:34:16 +0800

    Update db.php

--- project-folder/db.php
+++ project-folder/db.php
@@ -11,4 +11,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Execute query function using mysqli
+function executeQuery($query){
+    global $conn;
+    return $conn->query($query);  // Using $conn->query() for mysqli
+}
 ?>
