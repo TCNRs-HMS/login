930259636490b74d2e51fcf17e63c876b8d390b2
GROUP 3_Dalusung, Festejo, Isip, Sutare <153100461+TCNRs-HMS@users.noreply.github.com>
2024-11-18 18:38:57 +0800

    project-folder directory

--- project-folder/validation.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-    const username = document.querySelector("input[name='username']").value;
-    const password = document.querySelector("input[name='password']").value;
-
-    if (!username || !password) {
-        alert("Please fill out all fields!");
-        e.preventDefault();
-    }
-});
